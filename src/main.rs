f55228d2e127dc29694f6ef189c381ecbe422569
Tera3939 <tera3939.dev@gmail.com>
2016-10-27 12:14:18 +0900

    なぜそこで死ぬのか

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
+use std::collections::HashMap;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::sync::RwLock;
 use std::thread;
 
-fn login() {
-    let barner = b"Input your userID";
-    
+#[macro_use]
+extern crate lazy_static;
 
+lazy_static! {
+   static ref IDS: RwLock<HashMap<&'static str, TcpStream>> = {
+       let ids = HashMap::new();
+       RwLock::new(ids)
+   };
+}
+
+
+fn login(stream: &mut TcpStream) {
+    let banner = b"Input your UserID: ";
+    let _ = stream.write(banner);
+    let mut id = [0;128];
+    match stream.read(&mut id) {
+        Ok(n) => {
+            let id = std::str::from_utf8(&id[0..n]).unwrap();   // `id` does not live long enough
+            let mut ids = IDS.write().unwrap();
+            // UserIDとソケットを結びつけ、Thread間で共有する
+            ids.insert(id, *stream);                            // cannot move out of borrowed content
+        },
+        Err(why) => panic!("{:?}", why),
+    }
 }
 
 fn logout(){
@@ -17,19 +39,20 @@ fn send_all(){
 }
 
 fn handle_client(mut stream: TcpStream, counter: u8) {
-    // クライアントから送られてきたデータを受信
+    /// クライアントから送られてきたデータを受信して、methodに対応する関数を呼ぶ
+    /// protocol: method CRLF value
     loop{
         let mut buffer = [0;128];
         println!("{:p}", &stream);
-        // 受信できたか確認
         match stream.read(&mut buffer) {
             Ok(n) => {
                 if 0 < n {
-                    // プロトコルを確認
+                    // 受信したデータを&str型に変換
                     let method = std::str::from_utf8(&buffer[0..n]).unwrap();
                     println!("{:?}", method.split("\r\n").next().unwrap());
+                    // \r\nで分割してmethodを取り出す
                     match method.split("\r\n").next().unwrap() {
-                        "LOGIN" => login(),
+                        "LOGIN" => login(&mut stream),
                         "LOGOUT" => logout(),
                         "CAHT" => send_all(),
                         _ => println!("hoge:{}", counter),
@@ -44,7 +67,7 @@ fn handle_client(mut stream: TcpStream, counter: u8) {
 fn main() {
     // ソケットを生成
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
-    let mut counter = 0;
+    let mut thread_id = 0;
     println!("Start Listenig...");
     // accept()してるっぽい
     for stream in listener.incoming() {
@@ -52,10 +75,10 @@ fn main() {
         match stream {
             // incoming()がOk返したらhandle_clientのスレッドを生成
             Ok(mut stream) => {
-                counter += 1;
+                thread_id += 1;
                 println!("{:p}", &stream);
                 thread::spawn(move|| {
-                    handle_client(stream, counter)
+                    handle_client(stream, thread_id)
                 });
             }
             // Errが帰ってきたら頑張る
